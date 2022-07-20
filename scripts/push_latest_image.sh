4c31d769156258f034865c7968ef7a488312fb98
Lingnan Liu <liliu@confluent.io>
2022-07-20 10:30:33 -0700

    Rewrite push_latest_image script

--- scripts/push_latest_image.sh
+++ scripts/push_latest_image.sh
@@ -7,16 +7,24 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-trap 'rm -rf kubectl' EXIT
-trap 'rm -rf kubectl.sha256' EXIT
+trap 'docker image rm $TARGET_IMAGE' EXIT
 
-VERSION=$1
+# Get the image postfix from command line input
+TAG_POSTFIX=$1
+if [ -z $TAG_POSTFIX ]; then
+    echo "Usage: push_latest_image $TAG_POSTFIX"
+    exit 1
+fi
 
-curl -LO "https://dl.k8s.io/release/v$VERSION/bin/darwin/amd64/kubectl"
-curl -LO "https://dl.k8s.io/release/v$VERSION/bin/darwin/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | shasum -a 256 --check
+# Get the id, repository and tag of the latest image
+DOCKER_IMAGE_LS=$(docker image list --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}\t{{.Size}}" | sed -n 2p)
+read -r ID REPO TAG <<< $DOCKER_IMAGE_LS
 
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-sudo chown root: /usr/local/bin/kubectl
-kubectl version --client --output=yaml
+# Generates the image repository and tag for pushing
+TAG_PREFIX=$(awk 'BEGIN{FS=OFS="-"}{NF--; print}' <<< $TAG)
+TAG_WITH_POSTFIX="$TAG_PREFIX-$TAG_POSTFIX"
+TARGET_IMAGE="037803949979.dkr.ecr.us-west-2.amazonaws.com/$REPO:$TAG_WITH_POSTFIX"
+
+# Retag and push the image
+docker tag $ID $TARGET_IMAGE
+docker push $TARGET_IMAGE
