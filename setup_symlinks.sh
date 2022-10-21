5ee0ffb4397232164107e08e326484baf1e267c2
Lingnan Liu <liliu@confluent.io>
2022-10-21 16:11:32 -0700

    Separate Amethyst to MacOS only list

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 echo "Setting up symlinks with PWD = $PWD"
 
 originals=(
-	"$PWD/amethyst/amethyst.yml"
 	"$PWD/cheat"
 	"$PWD/git/gitconfig"
 	"$PWD/tmux/tmux.conf"
@@ -15,7 +14,6 @@ originals=(
 	"$PWD/wezterm/wezterm.lua"
 )
 links=(
-	"$HOME/.amethyst.yml"
 	"$HOME/.config/cheat"
 	"$HOME/.gitconfig"
 	"$HOME/.tmux.conf"
@@ -37,3 +35,31 @@ for index in ${!originals[*]}; do
 		echo "Created symlink: $original -> $link"
 	fi
 done
+
+setup_symlink_darwin() {
+	echo "Setting up symlinks for MacOS"
+	originals_darwin=(
+		"$PWD/amethyst/amethyst.yml"
+	)
+	links_darwin=(
+		"$HOME/.amethyst.yml"
+	)
+
+	for index in ${!originals_darwin[*]}; do
+		original=${originals_darwin[$index]}
+		link=${links_darwin[$index]}
+		echo "Setting up symlink: $original -> $link"
+		if [ -L "$link" ] && [ -e "$link" ]; then
+			echo "Found existing $link"
+		else
+			rm -rf "$link"
+			ln -sf "$original" "$link"
+			echo "Created symlink: $original -> $link"
+		fi
+	done
+}
+
+case "$OSTYPE" in
+darwin*) setup_symlink_darwin ;;
+*) echo "unknown: $OSTYPE" ;;
+esac
