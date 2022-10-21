2c5167c7ac7fdbaf38f2b05b0da64e9601b4508c
Lingnan Liu <liliu@confluent.io>
2022-10-21 11:40:25 -0700

    Automate adding common definitions

---
+++ setup_common_configurations.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+statement="# Common Definitions"
+echo "Adding statement: $statement"
+if rg -q "$statement" "$HOME/.zshrc"; then
+	echo "Found existing $statement"
+else
+	echo >>"$HOME/.zshrc"
+	echo "$statement" >>"$HOME/.zshrc"
+fi
+
+statements=(
+	"alias rfv=\"$PWD/zsh/rfv\""
+	"source \"$PWD/zsh/zshrc.common\""
+	"source \"$PWD/fzf-git/fzf-git.sh\""
+)
+
+for index in ${!statements[*]}; do
+	statement=${statements[$index]}
+	echo "Adding statement: $statement"
+	if rg -q "$statement" "$HOME/.zshrc"; then
+		echo "Found existing $statement"
+	else
+		echo "$statement" >>"$HOME/.zshrc"
+	fi
+done
