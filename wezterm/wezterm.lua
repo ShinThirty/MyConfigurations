4e5f4dc2e402a76a9c001a3ec62930f0a9af94ab
Lingnan Liu <liliu@confluent.io>
2022-10-31 19:58:11 -0700

    Wezterm hyperlink rules on github

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -131,6 +131,46 @@ wezterm.on("update-right-status", function(window, pane)
   window:set_right_status(wezterm.format(elements))
 end)
 
+local hyperlink_rules = {
+  -- Linkify things that look like URLs and the host has a TLD name.
+  -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+  {
+    regex = [[\b\w+://[\w.-]+\.[a-z]{2,15}\S*\b]],
+    format = "$0",
+  },
+
+  -- linkify email addresses
+  -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+  {
+    regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+    format = "mailto:$0",
+  },
+
+  -- file:// URI
+  -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+  {
+    regex = [[\bfile://\S*\b]],
+    format = "$0",
+  },
+
+  -- Linkify things that look like URLs with numeric addresses as hosts.
+  -- E.g. http://127.0.0.1:8000 for a local development server,
+  -- or http://192.168.1.1 for the web interface of many routers.
+  {
+    regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+    format = "$0",
+  },
+
+  -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+  -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+  -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+  -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+  {
+    regex = [[\bgit@github\.com:([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)\b]],
+    format = "https://www.github.com/$1/$3",
+  },
+}
+
 return {
   color_scheme = "Gruvbox dark, hard (base16)",
   colors = {
@@ -141,6 +181,7 @@ return {
   },
   font_size = 13.0,
   hide_tab_bar_if_only_one_tab = true,
+  hyperlink_rules = hyperlink_rules,
   line_height = 1.2,
   tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
