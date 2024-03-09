ef3fd904c75a9bc6d4b4b5e63587bb3919799e0d
Lingnan Liu <xmxu00@gmail.com>
2024-03-08 16:36:28 -0800

    feat: add script to access devel rw db

--- scripts/access_mothership_db.sh
+++ scripts/access_mothership_db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-access_mothership_db() {
+access_mothership_db_ro() {
     export CC_DOTFILES_BETA=true
     . /Users/lliu/Workspaces/cc-dotfiles/caas.sh
 
@@ -14,5 +14,23 @@ access_mothership_db() {
     # Create a ssh tunnel to the read only mothership database
     cctunnel db-ro -e ${ENVIRONMENT}
     # Output: connect to localhost:8432 to access the ENVIRONMENT mothership Read-Only DB
+    psql
+}
+
+access_mothership_db_rw() {
+    export CC_DOTFILES_BETA=true
+    . "$HOME"/Workspaces/cc-dotfiles/caas.sh
+
+    ENVIRONMENT=devel
+
+    # Refresh ssh key to the desired ENVIRONMENT if needed
+    assume 280640245378/nonprod-administrator
+    # or for stag or prod:
+    # assume 746494496109/nonprod-administrator
+
+    # Create an ssh tunnel to the read-write mothership database
+    cctunnel db-rw -e ${ENVIRONMENT} -p 8433
+    # Output: connect to localhost:8433 to access the ENVIRONMENT mothership DB
+    # Export the variables from the output
     psql
 }
