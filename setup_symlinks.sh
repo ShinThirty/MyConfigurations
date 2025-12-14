891f4dccfdc863d8bfe282ccb2afd25b21394f15
Lingnan Liu <xmxu00@gmail.com>
2025-12-13 18:42:39 -0800

    feat: add yazi, the terminal file manager

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -5,76 +5,80 @@ set -euo pipefail
 echo "Setting up symlinks with PWD = $PWD"
 
 originals=(
-    "$PWD/cheat"
-    "$PWD/git/gitconfig"
-    "$PWD/git/ignore"
-    "$PWD/ideavim/ideavimrc"
-    "$PWD/nano"
-    "$PWD/nvim"
-    "$PWD/vim"
-    "$PWD/p10k.zsh"
-    "$PWD/tmux/tmux.conf"
-    "$PWD/vale.ini"
-    "$PWD/ghostty"
-    "$PWD/zsh/zshrc"
+	"$PWD/cheat"
+	"$PWD/git/gitconfig"
+	"$PWD/git/ignore"
+	"$PWD/ideavim/ideavimrc"
+	"$PWD/nano"
+	"$PWD/nvim"
+	"$PWD/vim"
+	"$PWD/p10k.zsh"
+	"$PWD/tmux/tmux.conf"
+	"$PWD/vale.ini"
+	"$PWD/ghostty"
+	"$PWD/yazi/yazi.toml"
+	"$PWD/yazi/theme.toml"
+	"$PWD/zsh/zshrc"
 )
 links=(
-    "$HOME/.config/cheat"
-    "$HOME/.gitconfig"
-    "$HOME/.config/git/ignore"
-    "$HOME/.ideavimrc"
-    "$HOME/.config/nano"
-    "$HOME/.config/nvim"
-    "$HOME/.vim"
-    "$HOME/.p10k.zsh"
-    "$HOME/.tmux.conf"
-    "$HOME/.vale.ini"
-    "$HOME/.config/ghostty"
-    "$HOME/.zshrc"
+	"$HOME/.config/cheat"
+	"$HOME/.gitconfig"
+	"$HOME/.config/git/ignore"
+	"$HOME/.ideavimrc"
+	"$HOME/.config/nano"
+	"$HOME/.config/nvim"
+	"$HOME/.vim"
+	"$HOME/.p10k.zsh"
+	"$HOME/.tmux.conf"
+	"$HOME/.vale.ini"
+	"$HOME/.config/ghostty"
+	"$HOME/.config/yazi/yazi.toml"
+	"$HOME/.config/yazi/theme.toml"
+	"$HOME/.zshrc"
 )
 
 for index in ${!originals[*]}; do
-    original=${originals[$index]}
-    link=${links[$index]}
-    echo "Setting up symlink: $original -> $link"
-    if [ -L "$link" ] && [ -e "$link" ]; then
-        echo "Found existing $link"
-    else
-        rm -rf "$link"
-        ln -sf "$original" "$link"
-        echo "Created symlink: $original -> $link"
-    fi
+	original=${originals[$index]}
+	link=${links[$index]}
+	echo "Setting up symlink: $original -> $link"
+	if [ -L "$link" ] && [ -e "$link" ]; then
+		echo "Found existing $link"
+	else
+		rm -rf "$link"
+		ln -sf "$original" "$link"
+		echo "Created symlink: $original -> $link"
+	fi
 done
 
 setup_symlink_darwin() {
-    echo "Setting up symlinks for MacOS"
-    originals_darwin=(
-        "$PWD/amethyst/amethyst.yml"
-    )
-    links_darwin=(
-        "$HOME/.amethyst.yml"
-    )
+	echo "Setting up symlinks for MacOS"
+	originals_darwin=(
+		"$PWD/amethyst/amethyst.yml"
+	)
+	links_darwin=(
+		"$HOME/.amethyst.yml"
+	)
 
-    for index in ${!originals_darwin[*]}; do
-        original=${originals_darwin[$index]}
-        link=${links_darwin[$index]}
-        echo "Setting up symlink: $original -> $link"
-        if [ -L "$link" ] && [ -e "$link" ]; then
-            echo "Found existing $link"
-        else
-            rm -rf "$link"
-            ln -sf "$original" "$link"
-            echo "Created symlink: $original -> $link"
-        fi
-    done
+	for index in ${!originals_darwin[*]}; do
+		original=${originals_darwin[$index]}
+		link=${links_darwin[$index]}
+		echo "Setting up symlink: $original -> $link"
+		if [ -L "$link" ] && [ -e "$link" ]; then
+			echo "Found existing $link"
+		else
+			rm -rf "$link"
+			ln -sf "$original" "$link"
+			echo "Created symlink: $original -> $link"
+		fi
+	done
 }
 
 setup_symlink_linux() {
-    echo "Setting up symlinks for Linux"
+	echo "Setting up symlinks for Linux"
 }
 
 case "$OSTYPE" in
-    darwin*) setup_symlink_darwin ;;
-    linux*) setup_symlink_linux ;;
-    *) echo "unknown: $OSTYPE" ;;
+darwin*) setup_symlink_darwin ;;
+linux*) setup_symlink_linux ;;
+*) echo "unknown: $OSTYPE" ;;
 esac
