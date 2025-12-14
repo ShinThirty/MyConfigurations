9589f70caa16e6b1f5e9527da077035ce1d853fa
Lingnan Liu <xmxu00@gmail.com>
2025-12-13 18:42:39 -0800

    feat: add yazi, the terminal file manager

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -273,3 +273,12 @@ function omz_urldecode {
 
   echo -E "$decoded"
 }
+
+# yazi shell wrapper
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
